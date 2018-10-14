455ba2e24ecd46f237f92f6974f9dce37211fe1a
Sneha Mohan <sneha.mohan@harman.com>
2018-10-14 11:50:11 +0530

    My first commit in branch 1
    Signed-off-by: Sneha Mohan <sneha.mohan@harman.com>

--- test3.c
+++ test3.c
@@ -1,2 +1,2 @@
-I need to append this to the second commit as I would like to add that
-its far more intersting to learn.
+Hi,I need to append this to be  the second commit and I would like to add that
+its far more intersting to learn.This is easy.
