18955f3373e7a8b110f57049af2e109dc59db3c6
Sneha Mohan <sneha.mohan@harman.com>
2018-10-14 19:00:04 +0530

    Changes in test2
    Signed-off-by: Sneha Mohan <sneha.mohan@harman.com>

--- test3.c
+++ test3.c
@@ -1,2 +1,2 @@
-I need to append this to the second commit as I would like to add that
-its far more intersting to learn.
+I need to append this to second commit and I would like to add that
+its far more intersting to learn.I am loving this so far.
